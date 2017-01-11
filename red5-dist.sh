4f5fe239e716368cccca3ddc013b419213ef3997
telle <m.e.platova@gmail.com>
2017-01-11 15:04:20 +0700

    Add dist script. Update properties for our purposes.

---
+++ red5-dist.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#$1 type
+#$2 distribution directory, default: /usr/local/red5
+if [ -z $2 ]; then
+	dirName="/usr/local/red5"
+else
+	dirName=$2 || "/usr/local/red5/"
+fi
+
+echo "Installing Red5 into directory $dirName..."
+
+sudo rm -r $dirName/lib $dirName/plugins
+
+cd ../red5-io-fork
+mvn clean
+mvn -Dmaven.test.skip=true install || exit 1;
+
+cd ../red5-server-common-fork
+#mvn clean
+mvn -Dmaven.test.skip=true install || exit 1
+
+cd ../red5-server-fork
+mvn clean
+mvn -Dmaven.test.skip=true package -P assemble || exit 1 
+cd target/red5-server-*/red5-server/
+
+sudo mkdir $dirName                                                                                                                                                                                                               
+sudo mkdir $dirName/lib                                                                                                                                                                                                           
+sudo mkdir $dirName/plugins 
+
+case $1 in
+
+    update)
+    `sudo find . -name "*.jar" -exec cp {} $dirName/{} \;`
+    ;;
+    
+    dist)
+    `sudo cp -R . $dirName`
+    ;;
+
+    *)
+    echo 'Action is missing';
+    ;;
+esac
+
